9a5958758a78e12d9ff72b1d52c7434ce5de2245
thadclay <thadclay@gmail.com>
2014-07-09 10:13:58 -0400

    Refactored client and pgSetup.

--- test/integration/connection.test.js
+++ test/integration/connection.test.js
@@ -3,12 +3,20 @@
  */
 
 var expect = require('expect.js');
-var helper = require('../test_helper.js');
 var Client = require('../../lib/client');
 
 describe('connection', function() {
+  it('surfaces error in the callback when pool pg connection fails', function(done) {
+    var client = new Client({});
+    client.connect(true);
+    client.query('Select now();', function(err, result) {
+      expect(err).to.not.be.null;
+      done();
+    });
+  });
+
   it('surfaces error in the callback when pg connection fails', function(done) {
-    var client = new Client();
+    var client = new Client({});
     client.connect();
     client.query('Select now();', function(err, result) {
       expect(err).to.not.be.null;
